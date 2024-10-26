bcaf5cec32fca1fd3a45b47656f7d3123ac3665c
vaskivstanislav <stasmegatyr@gmail.com>
2024-10-26 16:39:29 +0300

    start

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Profile from "./Profile/Profile";
+import userData from "../userData.json";
+
+const App = () => {
+  return (
+    <div>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </div>
+  );
+};
+
+export default App;
